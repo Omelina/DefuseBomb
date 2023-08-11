d1c2f14af678972e0b9e8375b6811f778d6f95ea
omelina <okymelina98@gmail.com>
2023-08-11 15:32:13 -0600

    Tabla de Ranking

---
+++ DefuseBomb/RankingItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefuseBomb
+{
+    public class RankingItem
+    {
+        public string PlayerName { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ DefuseBomb/Views/RankPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DefuseBomb.Models;
+using DefuseBomb.ViewModels;
+using DefuseBomb.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DefuseBomb.Views
+{
+    public partial class RankPage : ContentPage
+    {
+
+        // Define la propiedad y el tipo con el mismo nivel de acceso.
+        public List<RankingItem> RankingList { get; set; }
+
+        public RankPage()
+        {
+            InitializeComponent();
+
+            // Crea una lista de ejemplo para mostrar en la tabla de ranking.
+            RankingList = new List<RankingItem>
+            {
+                new RankingItem { PlayerName = "Player 1", Points = 100 },
+                new RankingItem { PlayerName = "Player 2", Points = 90 },
+                new RankingItem { PlayerName = "Player 3", Points = 85 },
+                // Agrega más elementos según sea necesario...
+            };
+
+            // Establece el contexto de datos para el enlace de datos.
+            BindingContext = this;
+        }
+
+
+    }
+}
